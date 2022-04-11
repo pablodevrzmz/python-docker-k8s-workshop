10b7dd741d10488c5e6a31811e70ead231f8dcd2
Jose Pablo Ramírez Méndez <89355471+pablodevrzmz@users.noreply.github.com>
2022-04-10 21:31:02 -0600

    NEW: Adding functional docker files, docker compose and presentation

--- week 1/use-case/server/db/mongo_provider.py
+++ week 1/use-case/server/db/mongo_provider.py
@@ -1,13 +1,17 @@
 import pymongo
 import os
+import logging
 
 __con_str = "mongodb+srv://{0}:{1}@{2}/{3}?retryWrites=true&w=majority".format(
     os.getenv("MONGO_USER"),
-    os.getenv("MONGO_PASSWORD"),
+    os.getenv("MONGO_PASS"),
     os.getenv("MONGO_SERVER"),
     os.getenv("MONGO_DB"),
 )
 
 
-__client = pymongo.MongoClient(__con_str)
-db = __client[os.getenv("MONGO_DB")]
+try:
+    __client = pymongo.MongoClient(__con_str)
+    db = __client[os.getenv("MONGO_DB")]
+except Exception as e:
+    logging.info( str(e) )
--- week 1/use-case/server/main.py
+++ week 1/use-case/server/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routers.main_router import router as api_router
 import os
+import logging
 
 app = FastAPI()
 
@@ -18,5 +19,4 @@ app.add_middleware(
 
 app.include_router(api_router)
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8005, log_level="info", reload=True)
+#uvicorn main:app --host 0.0.0.0 --port 8005
